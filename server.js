e8f9b9f0641d6ae051d70d9b1242602dc179eded
mcchaves <eleceya.2@gmail.com>
2018-10-02 19:49:57 +0200

    creados header, footer, noticias y contactar hbs

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
 /*
@@ -9,7 +10,15 @@ const fileLogMiddleware = middleware.fileLogMiddleware
 // las tres líneas anteriores se reducen en una línea de la siguiente forma
 const { consoleLogMiddleware, fileLogMiddleware } = require('./middleware')
 
-app.get('/', (req, res) => { res.send('Hola Gente') })
+// configuro el hbs
+const hbs = require('hbs')
+hbs.registerPartials(path.join(__dirname, 'views', 'partials'))
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear())
+app.set('view engine', 'hbs')
+
+// indico la ruta donde tengo mi documento html
+const staticRoute = path.join(__dirname, 'public')
+app.use(express.static(staticRoute))
 
 // creamos un middleware que guarde traza de las fechas de accesos
 app.use(consoleLogMiddleware)
@@ -17,17 +26,25 @@ app.use(consoleLogMiddleware)
 // creo middleware que guarde los cambios en el fichero server.log
 app.use(fileLogMiddleware)
 
+app.get('/', (req, res) => { res.send('Hola Gente') })
+
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
 })
 
 app.get('/contacto', (req, res) => {
-  res.send('Página de contacto')
-})
-app.get('/contactar', (req, res) => {
-  res.send({ Nombre: 'Pepe', Correo: 'pepito@pepe.com' }) // guardo los datos de contacto
+  // res.send('Página de contacto')
+  // renderizo el html de contacto
+  res.render('contactar', {
+    pageTitle: 'Página de contacto'
 })
 
 app.get('/noticias', (req, res) => {
-  res.send('Página de noticias')
+  res.render('noticias', {
+    pageTitle: 'Página de noticias'
+  })
+})
+
+app.get('/contactar', (req, res) => {
+  res.send({ Nombre: 'Pepe', Correo: 'pepito@pepe.com' }) // guardo los datos de contacto
 })
